c7d858968abed5b9b314f9112a4b0b6afb8abd2b
ntzwilly <ntzwilly@gmail.com>
2021-11-29 13:20:27 +0200

    Add Gabumon to the animals table

--- data.sql
+++ data.sql
@@ -1,3 +1,4 @@
 /* Populate database with sample data. */
 
 INSERT INTO animals (name, date_of_birth, weight_kg) VALUES ('Agumon', '2020-2-3', 10.23);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Gabumon', '2018-11-15', 8, true, 2);
