40af3f4b4ea5dfabefb128b2456712be04b05c4a
ntzwilly <ntzwilly@gmail.com>
2021-11-29 14:48:19 +0200

    Find all animals not named Gabumon.

--- queries.sql
+++ queries.sql
@@ -12,3 +12,5 @@ SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 
 SELECT * FROM animals WHERE neutered = true;
+
+SELECT * FROM animals WHERE name != 'Gabumon';
