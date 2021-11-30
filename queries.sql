812b7dc438f346fa983cb0ec58d67ebf8c186c1d
ntzwilly <ntzwilly@gmail.com>
2021-11-30 14:24:47 +0200

    Final commit

--- queries.sql
+++ queries.sql
@@ -17,6 +17,8 @@ SELECT * FROM animals WHERE name != 'Gabumon';
 
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 
+
+
 BEGIN;
 UPDATE animals SET species = 'unspecified';
 SELECT * FROM animals;
