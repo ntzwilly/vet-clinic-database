05b9c288ca131cdb08853c28aa31c2e65b86769b
ntzwilly <ntzwilly@gmail.com>
2021-11-30 11:52:45 +0200

    Set species value to unspecified

--- queries.sql
+++ queries.sql
@@ -16,3 +16,7 @@ SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+COMMIT;
