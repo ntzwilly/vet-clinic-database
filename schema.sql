4c51a0f7f1c68f8f184707ab495faadacd8b39f4
ntzwilly <ntzwilly@gmail.com>
2021-12-01 19:02:15 +0200

    Define owner_id as a foreign key

--- schema.sql
+++ schema.sql
@@ -36,3 +36,5 @@ ALTER TABLE animals ADD COLUMN species_id INT;
 ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species (id);
 
 ALTER TABLE animals ADD COLUMN owner_id INT;
+
+ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners (id);
