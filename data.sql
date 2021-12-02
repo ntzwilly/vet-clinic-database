5a9cadee968e314d3f0d9db06594d139bc7246f1
ntzwilly <ntzwilly@gmail.com>
2021-12-02 13:05:03 +0200

    Add Angemon visit to Jack Harkness

--- data.sql
+++ data.sql
@@ -67,3 +67,4 @@ INSERT INTO visits (animal_id, vet_id, date_of_visit) VALUES (11, 1, '2020-8-10'
 INSERT INTO visits (animal_id, vet_id, date_of_visit) VALUES (11, 2, '2021-4-7');
 INSERT INTO visits (animal_id, vet_id, date_of_visit) VALUES (7, 3, '2019-9-29');
 INSERT INTO visits (animal_id, vet_id, date_of_visit) VALUES (8, 4, '2020-10-3');
+INSERT INTO visits (animal_id, vet_id, date_of_visit) VALUES (8, 4, '2020-11-4');
