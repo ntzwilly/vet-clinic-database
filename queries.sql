13534e8f20fa8c971e44420b261d48a3ffc1e605
ntzwilly <ntzwilly@gmail.com>
2021-12-02 16:16:23 +0200

    Select the animal who was Maisy Smith's first visit

--- queries.sql
+++ queries.sql
@@ -116,3 +116,8 @@ SELECT name, COUNT(*) FROM animals
     INNER JOIN visits ON visits.animal_id = animals.id
     GROUP BY animals.name
     ORDER BY COUNT(*) DESC LIMIT 1;
+
+SELECT animals.name, visits.date_of_visit FROM animals 
+    INNER JOIN visits ON visits.animal_id = animals.id 
+    INNER JOIN vets ON vets.id = visits.vet_id 
+    WHERE vets.name = 'Maisy Smith' ORDER BY date_of_visit ASC LIMIT 1;
